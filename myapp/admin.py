d44668751eebfad2ccda1fa0b4e117d30efaaa38
Ali Arshad <aliarshad.cs@gmail.com>
2021-08-30 00:11:11 +0500

    added new app

---
+++ myapp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
+from .models import Resume
+
+# Register your models here.
+@admin.register(Resume)
+class ResumeModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'dob', 'gender', 'locality', 'city', 'pin', 'state', 'mobile', 'job_city', 'profile_image', 'my_file']
+
